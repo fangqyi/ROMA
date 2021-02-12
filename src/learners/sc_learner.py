d6fc9c97c32a9f51f35106db045d847b5f2ee2c2
QIAOYI FANG <QF30@duke.edu>
2021-02-12 02:41:08 -0500

    fixed update rule for actor

--- src/learners/sc_learner.py
+++ src/learners/sc_learner.py
@@ -128,11 +128,10 @@ class SCLearner:
         q_vals = q_vals[:, :-self.args.horizon]
         q_vals = q_vals.reshape(-1, 1)
         lstm_r = lstm_r.reshape(-1, 1)
+        pi = lstm_out.reshape(-1, 1)
 
         mask = mask.repeat(1, 1, self.n_agents).view(-1)
-        print("lstm_out {}".format(lstm_out.shape))
-        print("actions {}".format(actions.shape))
-        pi_taken = torch.gather(lstm_out, dim=1, index=actions.reshape(bs, -1, 1)).squeeze(1)
+        pi_taken = torch.gather(pi, dim=1, index=actions.reshape(-1, 1)).squeeze(1)
         pi_taken[mask == 0] = 1.0
         pi_taken = torch.prod(pi_taken, dim=1).squeeze(1)
         log_pi_taken = torch.log(pi_taken)
