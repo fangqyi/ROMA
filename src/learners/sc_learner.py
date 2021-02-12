afd5119d0a395d8b0e59a8b15b7a0383847558f8
QIAOYI FANG <QF30@duke.edu>
2021-02-12 02:09:44 -0500

    fixed a small thing

--- src/learners/sc_learner.py
+++ src/learners/sc_learner.py
@@ -185,9 +185,9 @@ class SCLearner:
     def _get_dlistm_partial(self, dirs_vals, queries, keys, rules, t):
         # calculate dcos(zt+c−zt,gt(θ)) in multi-agent context
 
-        print("dirs_vals shape: {}".format(dirs_vals.shape))
+        # print("dirs_vals shape: {}".format(dirs_vals.shape))
         dir_vals_t = dirs_vals[:, t]
-        dir_vals_tc = dir_vals_t[:, t+self.args.horizon]
+        dir_vals_tc = dirs_vals[:, t+self.args.horizon]
         query = queries[:, t]
         key = keys[:, t]
         rule = keys[:, t]
